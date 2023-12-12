351a4eba5ded86eeecc6a35bb619265967c072c3
Damian Trowski <damian.dt.pl@gamil.com>
2023-12-12 22:59:08 +0100

    nwm

--- amicable_numbers.hs
+++ amicable_numbers.hs
@@ -1,26 +1,27 @@
+import Control.Applicative (Alternative(empty))
 sumaDzielnikow :: Int -> Int
-sumaDzielnikow x =
-  sum
-    [ d
-      | d <- [1 .. x - 1],
-        x `mod` d == 0
-    ]
+sumaDzielnikow x = 
+ sum (
+  [ d | d <- [1..sqrl], x `mod` d == 0, d*d<=x] 
+  ++ 
+  [ x `div` d | d <- [2 .. sqrl], x `mod` d == 0, d*d<x]
+  ) 
+ where sqrl = floor (sqrt (fromIntegral x)) + 1
 
-paryZaprzyjaznione :: Int -> (Int, Int)
-paryZaprzyjaznione n =
-  head
-    [ (x, y)
-      | x <- [n - 1, n - 2 .. 1],
-        let y = sumaDzielnikow x,
-        y < x,
-        sumaDzielnikow y == x
-    ]
+paryZaprzyjaznione :: Int -> [(Int, Int)]
+paryZaprzyjaznione n = [ 
+ (x, y)
+ | x <- [n, n - 1 .. 1],
+ let y = sumaDzielnikow x,
+ y < x, sumaDzielnikow y == x
+ ]
 
 main :: IO ()
 main = do
-  putStrLn "Wprowadź n:"
-  n <- readLn :: IO Int
-  if n < 284
-    then putStrLn ("Dla n=" ++ show n ++ " nie ma par liczb zaprzyjaźnionych")
-    else do
-      print (paryZaprzyjaznione n)
+ putStrLn "Wprowadź n:"
+ n <- readLn :: IO Int
+ let pary = paryZaprzyjaznione n
+ if null pary
+  then putStrLn ("Dla n=" ++ show n ++ " nie ma par liczb zaprzyjaźnionych")
+  else do
+   print (head pary)
