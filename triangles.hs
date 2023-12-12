2c92215c2fb165954c833c64f157d52e7f2bdfc9
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2023-12-12 15:01:27 +0100

    Delete comments

--- triangles.hs
+++ triangles.hs
@@ -1,10 +1,5 @@
 import Data.List (nub, sort)
 
--- For a given natural number n, give such a number x ≤ n that can be decomposed into the largest number of
--- different triples a, b, c. Such that a + b + c = x and from sides of length a, b, c can be built a triangle
--- Rectangular. In the case of several maximum values of x, return all of them. For example,
--- for n=100 the numbers 60, 84 and 90 each have 2 distributions. Then the result should be: [60, 84, 90]
-
 isRightTriangle :: (Integer, Integer, Integer) -> Bool
 isRightTriangle (a, b, c) = a ^ 2 + b ^ 2 == c ^ 2
 
@@ -22,7 +17,6 @@ generateTriplesWithMaxSums n = nub $ filter (\x -> length (filter (== x) sums) =
 
 main :: IO ()
 main = do
-  -- Input n in the console
   putStrLn "Enter n:"
   n <- readLn :: IO Integer
   putStrLn $ "n=" ++ show n ++ ", result: " ++ show (generateTriplesWithMaxSums n)
