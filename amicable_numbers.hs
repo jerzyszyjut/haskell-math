461c5f52a37a90df255a1ee6fb2ed9c575887d52
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2023-12-12 20:59:41 +0100

    Improve amicable numbers code

--- amicable_numbers.hs
+++ amicable_numbers.hs
@@ -7,19 +7,20 @@ sumaDzielnikow x =
     ]
 
 paryZaprzyjaznione :: Int -> (Int, Int)
-paryZaprzyjaznione n
-  | n < 284 = (0, 0)
-  | otherwise =
-      head
-        [ (x, y)
-          | x <- [n - 1, n - 2 .. 1],
-            let y = sumaDzielnikow x,
-            y < x,
-            sumaDzielnikow y == x
-        ]
+paryZaprzyjaznione n =
+  head
+    [ (x, y)
+      | x <- [n - 1, n - 2 .. 1],
+        let y = sumaDzielnikow x,
+        y < x,
+        sumaDzielnikow y == x
+    ]
 
 main :: IO ()
 main = do
-  putStrLn "Enter n:"
+  putStrLn "Wprowadź n:"
   n <- readLn :: IO Int
-  print (paryZaprzyjaznione n)
+  if n < 284
+    then putStrLn ("Dla n=" ++ show n ++ " nie ma par liczb zaprzyjaźnionych")
+    else do
+      print (paryZaprzyjaznione n)
